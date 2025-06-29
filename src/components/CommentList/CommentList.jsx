a784f0e19628fffe1e4e57f2e5b95e64d5a1423a
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-29 16:56:33 +0300

    add a solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,13 +1,15 @@
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => {
-  return (
-    comments.length > 0 && (
-      <div className="CommentList">
-        {comments.map(comment => (
-          <CommentInfo comment={comment} key={comment.id} />
-        ))}
-      </div>
-    )
+  return comments.length > 0 ? (
+    <div className="CommentList">
+      {comments.map(comment => (
+        <CommentInfo comment={comment} key={comment.id} />
+      ))}
+    </div>
+  ) : (
+    <div data-cy="NoCommentsMessage" className="CommentList">
+      No comments
+    </div>
   );
 };
